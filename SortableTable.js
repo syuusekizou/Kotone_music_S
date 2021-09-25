c7942382e2ae4c0cd305b2a48e97dbc7245c1370
syuusekizou <croissant.craft.boss@gmail.com>
2021-09-25 22:01:12 +0900

    Add files via upload
    新規作成

---
+++ SortableTable.js
@@ -0,0 +1,36 @@
+const sortObj = new SortableTable();
+
+//並び変えるテーブルを設定 
+sortObj.setTable(document.querySelector('#my-table1'));
+
+//id=urlの項目にリンクを設定
+sortObj.setCellRenderer((col, row) => {
+    const colValue = row[col.id];
+    // cell-is-a-header
+    if (col.isHeader) {
+        if (typeof colValue !== 'undefined') {
+            return `<th>${colValue}</th>`;
+        }
+        return '<th></th>';
+    }
+    // cell-is-not-a-header
+    if (typeof colValue !== 'undefined') {
+        if (col.id === 'url') {
+            return `<td><a href="${colValue}" target="_blank">${colValue}</a></td>`;
+        }
+        return `<td>${colValue}</td>`;
+    }
+    return '<td></td>';
+});
+
+//データ設定
+let items = [];
+let index = 1;
+singableList.forEach(x => items.push({ id: index++, classification: x[0], name: x[1] }));
+sortObj.setData(items);
+
+// button handlers
+document.querySelector('#btnReset')
+    .addEventListener('click', () => {
+        sortObj.resetData();
+    });
